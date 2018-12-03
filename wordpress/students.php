c5806b923b5a23aed1bfaac59473cc5638f68442
Charleston Telles <charlestontelles@gmail.com>
2018-12-03 08:58:33 -0200

    Create students.php

---
+++ wordpress/students.php
@@ -0,0 +1,33 @@
+<?php /* Template Name: STUDENTS */
+/**
+ * The template for displaying all students
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+get_header(); ?>
+
+<?php echo “STUDENTS LIST”;?>
+<?php
+
+global $wpdb;
+$students = $wpdb->get_results("SELECT * FROM SENAI.STUDENTS;");
+
+echo "<br>";
+echo "<table>";
+foreach($students as $student){
+echo "<tr>";
+echo "<td>".$student->NAME."</td>";
+echo "<td>".$student->EMAIL."</td>";
+echo "<td>".$student->PHONE."</td>";
+echo "<td>".$student->COURSE."</td>";
+echo "</tr>";
+}
+echo "</table>";
+?>
+
+<?php get_footer();
